f4504a5347b0c5b9c76f88808ca0fdf156078be4
yijungai <ethan.yijun@gmail.com>
2020-07-29 21:31:34 +1000

    rebuild after fix

---
+++ public/build/precache-manifest.48fa43fee06424197770cddcff4bfbb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8436bfb503d45245a7bce9aaaed2c962",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d31ef7cd4b63cfde146",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "a15aff6beadf472afa15",
+    "url": "/static/js/2.e950a188.chunk.js"
+  },
+  {
+    "revision": "b74364752c6386920aced16a152e4b46",
+    "url": "/static/js/2.e950a188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d31ef7cd4b63cfde146",
+    "url": "/static/js/main.83ca357a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
