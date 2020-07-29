c53a0cece7cdd8a01d318de16ef095cbd8955fac
yijungai <ethan.yijun@gmail.com>
2020-07-29 19:43:03 +1000

    add and track the client

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./App.css";
+import logo from "./logo.png";
+import { ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import Launches from "./components/Launches";
+import Launch from "./components/Launch";
+import { Component } from "react";
+import { ApolloProvider as ApolloHooksProvider } from "@apollo/react-hooks";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// const { ApolloServer } = require("apollo-server");
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <ApolloHooksProvider client={client}>
+          <Router>
+            <div className="container">
+              <img
+                src={logo}
+                alt="SpaceX"
+                style={{ width: 300, display: "block", margin: "auto" }}
+              />
+              <Route exact path="/" component={Launches} />
+              <Route exact path="/launch/:flight_number" component={Launch} />
+            </div>
+          </Router>
+        </ApolloHooksProvider>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
