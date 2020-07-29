4b06071ad00c86ef4fa5ee5b8cc9c45ac148cd93
yijungai <ethan.yijun@gmail.com>
2020-07-29 21:17:01 +1000

    fix naming issue and span tag displaying issue

--- client/src/components/LaunchItem.js
+++ client/src/components/LaunchItem.js
@@ -4,7 +4,7 @@ import Moment from "react-moment";
 import { Link } from "react-router-dom";
 
 export default function LaunchItem({
-  launch: { flight_number, name, launch_year, date_local, success },
+  launch: { flight_number, mission_name, launch_date_local, launch_success },
 }) {
   return (
     <div className="card card-body mb-3">
@@ -14,15 +14,15 @@ export default function LaunchItem({
             Mission name:{" "}
             <span
               className={classNames({
-                "text-success": success,
-                "text-danger": !success,
+                "text-success": launch_success,
+                "text-danger": !launch_success,
               })}
             >
-              {name}
+              {mission_name}
             </span>
           </h4>
           <p>
-            Date: <Moment format="YYYY-MM-DD HH:mm">{date_local}</Moment>
+            Date: <Moment format="YYYY-MM-DD HH:mm">{launch_date_local}</Moment>
           </p>
         </div>
         <div className="col-md-3">
